4dacf01be8e18cab2e45c4bfd491adf168110d9e
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-04-17 02:24:40 +0600

    route setup and initial pages created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './pages/Blogs/Blogs';
+import Home from './pages/Home/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="">
+
+
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="blogs" element={<Blogs></Blogs>} />
+        <Route path="*" element={<NotFound></NotFound>} />
+      </Routes>
 
     </div>
   );
