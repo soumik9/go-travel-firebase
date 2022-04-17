f9a3bb8b3c44ddf2b78ae3971235461375da119f
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-04-17 21:29:26 +0600

    services title added

--- src/pages/Home/Serivces/Services/Services.js
+++ src/pages/Home/Serivces/Services/Services.js
@@ -16,6 +16,11 @@ const Services = () => {
     return (
         <section className='services__container my-5'>
             <Container>
+
+                <div className="d-flex justify-content-center services__container-title mb-3">
+                    <h2>Our Services</h2>
+                </div>
+
                 <Row>
 
                     {
