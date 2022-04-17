94a6c66bb252e71d9d0ea2507b5cf53171653076
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-04-17 21:45:46 +0600

    footer social media icons added

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './footer.css'
+import {AiOutlineInstagram, AiOutlineWhatsApp} from 'react-icons/ai'
+import {RiFacebookCircleLine} from 'react-icons/ri'
+import {FiTwitter} from 'react-icons/fi'
 
 const Footer = () => {
     return (
         <footer className='d-flex justify-content-center align-items-center'>
             <div className="footer__items">
-                <div className="social__icons">
-
+                <div className="social__icons d-flex justify-content-around align-items-center mb-3">
+                    <div><RiFacebookCircleLine className='footer__icons' /></div>
+                    <div><AiOutlineInstagram className='footer__icons' /></div>
+                    <div><FiTwitter className='footer__icons' /></div>
+                    <div><AiOutlineWhatsApp className='footer__icons' /></div>
                 </div>
                 <div className="copyright">
-                    <p>Copyright &copy; GO TRAVELS {new Date().getFullYear()}</p>
+                    <p>Copyright &copy; <b><i>GO T</i>RAVELS</b> {new Date().getFullYear()}</p>
                 </div>
             </div>
         </footer>
