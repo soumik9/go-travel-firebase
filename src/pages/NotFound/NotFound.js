22d0f2495dd28e64fe32832d7c7b01d907eb1bb3
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-04-18 17:45:04 +0600

    404 page added

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='not__found__container'>
+            <Container>
+                <Row className='justify-content-around'>
+                    <Col md={10}>
+
+                        <img className='img-fluid' style={{ height:"467px" }} src="https://i.ibb.co/4fkHSbT/404-pages.jpg" alt="" />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
